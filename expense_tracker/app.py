e4d9a86788d5e92d38d173ae735df9b851b0ecba
Uldis Upāns <uldis.upans@gmail.com>
2026-05-15 17:56:30 +0300

    feat: add filtering by month

--- expense_tracker/app.py
+++ expense_tracker/app.py
@@ -1,6 +1,6 @@
 from storage import load_expenses, save_expenses
-from logic import sum_total, sum_by_category
-from ui import wait_for_user, show_menu, get_new_expense, display_expenses, display_category_expenses
+from logic import sum_total, sum_by_category, get_available_months, filter_by_month
+from ui import wait_for_user, show_menu, get_new_expense, display_expenses, display_category_expenses, ask_month_selection, clear_screen
 
 CATEGORIES = ["Ēdiens", "Transports", "Izklaide",
     "Komunālie maksājumi", "Veselība", "Iepirkšanās", "Cits"]
@@ -24,6 +24,22 @@ def main():
             display_expenses(expenses)  
             wait_for_user()    
 
+        if choice == "3":   # Filtrēt pēc mēneša
+            # 1. Loģikas modulis atrod pieejamos mēnešus
+            available_months = get_available_months(expenses)
+            
+            # 2. UI modulis nodarbojas ar parādīšanu un izvēli
+            selected_month = ask_month_selection(available_months)
+            
+            if selected_month:
+                # 3. Loģikas modulis nofiltrē datus
+                filtered_data = filter_by_month(expenses, selected_month)
+                
+                # 4. UI modulis parāda rezultātus
+                print(f"--- Izdevumi mēnesī: {selected_month} ---")
+                display_expenses(filtered_data, selected_month)
+                wait_for_user()            
+
         if choice == "4":   # Kopsavilkums pa kategorijām
             # 1. Veicam aprēķinus, izmantojot loģikas moduli
             summary_data = sum_by_category(expenses)
@@ -33,6 +49,7 @@ def main():
             wait_for_user()
 
         elif choice == "7": # Iziet
+            clear_screen()
             print("Uz redzēšanos!")
             break
 
